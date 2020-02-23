f1033388a511e9780be260ee513c61bab1f1c02f
vladimirKartyshov <vovan198080@inbox.ru>
2020-02-23 22:04:17 +0300

    Json запрос

--- src/main/java/com/example/movies/activities/MainActivity.java
+++ src/main/java/com/example/movies/activities/MainActivity.java
@@ -3,16 +3,71 @@ package com.example.movies.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.movies.R;
+import com.example.movies.data.MovieAdapter;
+import com.example.movies.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.hasFixedSize();//для улучшения производительности
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movies = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+
+        getMovies();//при помощи этого метода будем распознавать JSON обЪект
     }
 
+    private void getMovies() {
+
+        String url = "http://www.omdbapi.com/?apikey=3b8a83ef&s=superman";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                JSONArray jsonArray = response.getJSONArray("Search");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();//метод чтобы при ошибке stack ошибок был выведен
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
 
 }
